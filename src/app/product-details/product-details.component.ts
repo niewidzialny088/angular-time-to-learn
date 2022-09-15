9193eaae15dd68ac474e405fc39d9967776fdc3e
niewidzialny088 <107325914+niewidzialny088@users.noreply.github.com>
2022-09-15 14:20:05 +0200

    1 file modified

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -23,7 +23,8 @@ export class ProductDetailsComponent implements OnInit {
   // Find the product that correspond with the id provided in route.
   this.product = products.find(product => product.id === productIdFromRoute);
   }
+
   addToCart(product: Product) {
     this.cartService.addToCart(product);
-    window.alert('Your product has been added to the cart!');
+    window.alert('Your product has been added to the cart!');}
 }
