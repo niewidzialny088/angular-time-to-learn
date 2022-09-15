43c21c3fba676771b8c43563d083e79b6c9ee898
niewidzialny088 <107325914+niewidzialny088@users.noreply.github.com>
2022-09-15 14:36:04 +0200

    1 file modified, 1 added

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -7,24 +7,28 @@ import { CartService } from '../cart.service';
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
-  styleUrls: ['./product-details.component.css']
+  styleUrls: ['./product-details.component.css'],
 })
 export class ProductDetailsComponent implements OnInit {
-
   product: Product | undefined;
-  constructor(private route: ActivatedRoute,
-  private cartService: CartService) { }
+  constructor(
+    private route: ActivatedRoute,
+    private cartService: CartService
+  ) {}
 
   ngOnInit(): void {
     // First get the product id from the current route.
-  const routeParams = this.route.snapshot.paramMap;
-  const productIdFromRoute = Number(routeParams.get('productId'));
+    const routeParams = this.route.snapshot.paramMap;
+    const productIdFromRoute = Number(routeParams.get('productId'));
 
-  // Find the product that correspond with the id provided in route.
-  this.product = products.find(product => product.id === productIdFromRoute);
+    // Find the product that correspond with the id provided in route.
+    this.product = products.find(
+      (product) => product.id === productIdFromRoute
+    );
   }
 
   addToCart(product: Product) {
     this.cartService.addToCart(product);
-    window.alert('Twój product zostął dodany do koszyczka');}
+    window.alert('Twój product zostął dodany do koszyczka');
+  }
 }
